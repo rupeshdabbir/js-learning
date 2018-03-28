a7a786fbb42563b44e93e4673ca4bf33d65e521b
msiddharthreddy <msiddharthreddy@github.com>
2018-03-28 14:41:18 -0700

    Filter in arrays

---
+++ app/js/Arrays/filter.js
@@ -0,0 +1,20 @@
+Array.prototype.filter = function(cb) {
+    if(!cb) {
+        throw new Error("No cb")
+    }
+    let result = [];
+    for(let i =0; i< Object(this).length; i++) {
+        if(cb(Object(this)[i], i)) {
+            result.push(Object(this)[i]);
+        };
+    }
+    return result;
+}
+
+
+var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
